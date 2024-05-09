8ca57e8cabecb888a646a3f12d50e8f0b4e3605f
Tytyaz2 <120456383+Tytyaz2@users.noreply.github.com>
2024-05-09 14:14:39 +0200

    architecture MVC model(code dur) views(graphic) Controllers(lien entre view et model)

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import main.views.Connexion;
+
+public class Main {
+    public static void main(String[] args) {
+
+        // Lancer l'application JavaFX en appelant la méthode launch()
+        Application.launch(Connexion.class, args);
+    }
+}
+
--- src/main/views/Connexion.java
+++ src/main/views/Connexion.java
@@ -1,14 +1,13 @@
-package main;
+package main.views;
 
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Connexion extends Application {
 
     private Stage primaryStage;
     private VBox root;
@@ -19,7 +18,7 @@ public class Main extends Application {
 
         // Créer le bandeau CYBOOKS
         Label labelCybooks = new Label("CYBOOKS");
-        labelCybooks.setStyle("-fx-font-size: 24pt; -fx-font-weight: bold;");
+        labelCybooks.setStyle("-fx-font-size: 24pt; -fx-font-weight: bold;top:0%;");
 
         // Créer le conteneur racine
         root = new VBox();
