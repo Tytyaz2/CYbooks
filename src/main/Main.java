3bc64924cfbbe035d6a1ef50cb3fb80cc4851809
Tytyaz2 <120456383+Tytyaz2@users.noreply.github.com>
2024-05-09 12:42:19 +0200

    javaFX Cybooks

---
+++ src/main/Main.java
@@ -0,0 +1,41 @@
+package main;
+
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    private Stage primaryStage;
+    private VBox root;
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+
+        // Créer le bandeau CYBOOKS
+        Label labelCybooks = new Label("CYBOOKS");
+        labelCybooks.setStyle("-fx-font-size: 24pt; -fx-font-weight: bold;");
+
+        // Créer le conteneur racine
+        root = new VBox();
+        root.setAlignment(Pos.CENTER);
+        root.getChildren().add(labelCybooks);
+
+        // Créer la scène principale
+        Scene scene = new Scene(root, 1920, 1080);
+
+        // Configurer et afficher la scène principale
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("CYBOOKS Application");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
