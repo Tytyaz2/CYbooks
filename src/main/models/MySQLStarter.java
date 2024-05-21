5e3702fc43e6ddf9248d85ca0826c25469043f8a
Nadirici <nadir1401@gmail.com>
2024-05-21 16:54:32 +0200

    Veuillez installer wamp et l'utiliser puisque XAMPP à tendance à faire crash

--- src/main/models/MySQLStarter.java
+++ src/main/models/MySQLStarter.java
@@ -6,18 +6,32 @@ import java.io.InputStreamReader;
 public class MySQLStarter {
 
     public static void startMySQL() {
-        String[] command = {"cmd.exe", "/c", "net start mysql"};
+        String[] command = {"cmd.exe", "/c", "net start wampmysqld64 "};
+        String[] command2 = {"cmd.exe", "/c", "net start wampapache64 "};
 
         try {
+            ProcessBuilder pb2 = new ProcessBuilder(command2);
             ProcessBuilder pb = new ProcessBuilder(command);
+            pb2.redirectErrorStream(true);
             pb.redirectErrorStream(true);
+            Process process2 = pb2.start();
             Process process = pb.start();
 
+            BufferedReader reader2 = new BufferedReader(new InputStreamReader(process2.getInputStream()));
             BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line2;
+            while ((line2 = reader2.readLine()) != null){
+                System.out.println(line2);
+
+            }
+
             String line;
             while ((line = reader.readLine()) != null) {
                 System.out.println(line);
             }
+
+
             process.waitFor();
         } catch (Exception e) {
             e.printStackTrace();
